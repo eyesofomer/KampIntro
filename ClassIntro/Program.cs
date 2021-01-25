6725cb2e2e2c78023b815135a04f9ba98ff09b11
Abdullah Kara <kara_abdullah34@hotmail.com>
2021-01-26 01:22:24 +0300

    26.01.2020.Gitignore ve .gitattributes ekle.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ClassIntro
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string adi = "Mehmet";
+			int yas = 25;
+
+			Kurs kurs1 = new Kurs();
+			kurs1.KursAdi = "C#";
+			kurs1.Egitmen = "Fırat Demir";
+			kurs1.IzlenmeOrani = 68;
+
+
+			Kurs kurs2 = new Kurs();
+			kurs2.KursAdi = "Python";
+			kurs2.Egitmen = "Alp Kağan";
+			kurs2.IzlenmeOrani = 72;
+
+
+			Kurs kurs3 = new Kurs();
+			kurs3.KursAdi = "Java";
+			kurs3.Egitmen = "Salih Bahçe";
+			kurs3.IzlenmeOrani = 55;
+
+
+			Kurs kurs4 = new Kurs();
+			kurs4.KursAdi = "C++";
+			kurs4.Egitmen = "Ali Kaçar";
+			kurs4.IzlenmeOrani = 89;
+
+			//Console.WriteLine(kurs1.KursAdi + " " + kurs1.Egitmen);
+
+			Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3, kurs4 };
+
+			foreach (var kurs in kurslar)
+			{
+				Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+			}
+
+
+
+
+			//Console.WriteLine("Hello World!");
+		}
+	}
+
+	class Kurs
+	{
+		public string KursAdi { get; set; }
+		public string Egitmen { get; set; }
+		public int IzlenmeOrani { get; set; }
+	}
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Loops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string kurs1 = "Yazılım Geliştiricis Yetiştirme Kampı";
+			string kurs2 = "Programlamaya başlangıç için temel kurs";
+			string kurs3 = "Java";
+			string kurs4 = "Python";
+
+			Console.WriteLine(kurs1);
+			Console.WriteLine(kurs2);
+			Console.WriteLine(kurs3);
+			Console.WriteLine(kurs4);
+
+			//array = dizi
+
+			string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı",
+				"Programlamaya başlangıç için temel kurs", "Java", "Python", "C#" };
+
+			
+			for (int i = 0; i < kurslar.Length; i++)
+			{
+				Console.WriteLine(kurslar[i]);
+			}
+
+			Console.WriteLine("For bitti");
+			
+			//foreach dizi içerisindeki her bir elemanı için kullanılır
+			foreach (string kurs in kurslar) //kurs: takma ad
+			{
+				Console.WriteLine(kurs);
+			}
+
+			Console.WriteLine("sayfa sonu - footer");
+		}
+	}
+}
