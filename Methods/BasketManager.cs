4bfae3f14f21204cc03701d1be7679b9bacb93ff
Abdullah Kara <kara_abdullah34@hotmail.com>
2021-01-26 23:31:00 +0300

    16.01.2021 Ders sonu

---
+++ Methods/BasketManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class BasketManager
+    {
+        //naming convention - isimlendirme kuralı
+        //syntax
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Tebrikler, Sepete eklendi : " + urun.Adi+" "+ urun.Fiyati);
+        }
+
+        public void Ekle2(string urunAdi, string Aciklama, double fiyat, int stokAdedi) 
+        {
+            Console.WriteLine("Tebrikler, Sepete eklendi : " + urunAdi);
+        }
+
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] {"Elma", "Karpuz" };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Portakal";
+            urun1.Fiyati = 7.5;
+            urun1.Aciklama = "Washington portakalı";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Erik";
+            urun2.Fiyati = 40;
+            urun2.Aciklama = "Hatay eriği";
+
+            Product[] urunler = new Product[] {urun1, urun2 };
+
+            //type-safe -- tip güvenliği
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-----------------------");
+            }
+
+            Console.WriteLine("------------------Methods----------------------");
+            //instance - örnek
+            //encapsulation
+
+            BasketManager sepetManager = new BasketManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 11, 6);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 7, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 10, 4);
+
+        }
+    }
+}
+//Method, tekrar tekrar kullanılabilirliği sağlayan prensip sağlar
+//Dont repeat yourself - DRY - Clean code - Best Practice
