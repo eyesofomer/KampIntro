7d3a2795dff85ae3ed2e7c7b56f4bfcee4e6dc6b
Abdullah Kara <kara_abdullah34@hotmail.com>
2021-02-01 00:21:24 +0300

    20 Ocak 2020 Ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{//CRUD Operasyonlarında CREATE, READ, UPDATE, DELETE komutları yer alır
+    class ProductManager //CRUD Operasyonları içerir
+    {
+     public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }  
+
+     public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+
+        }
+
+        
+    }
+}
