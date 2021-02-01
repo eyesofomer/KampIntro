ddd94ee1b8911ecfbdfbaa85a927c237945ceb6a
Abdullah Kara <kara_abdullah34@hotmail.com>
2021-02-01 22:39:45 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/CorporateCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //İnheritance - Miras
+    class CorporateCustomer:Customer
+    {
+        
+        public string CompanyName { get; set; }
+        public string TaxNumber { get; set; }
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP2
+{
+    //Individual
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.CustomerNumber = "12345";
+            customer1.Name = "Ahmet";
+            customer1.Surname = "Şahin";
+            customer1.TcNumber = "12345678910";
+
+
+            //Kodlama.io
+
+            CorporateCustomer customer2 = new CorporateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNumber = "54321";
+            customer2.CompanyName = "Kodlama.io";
+            customer2.TaxNumber="1234567890";
+
+
+            //Gerçek Müşteri - Tüzel Müşteri
+            //SOLID
+
+            Customer customer3 = new IndividualCustomer();
+            Customer customer4 = new CorporateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+
+        }
+    }
+}
